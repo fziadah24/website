278a97c8922475b7eb6cbabfa19ba34875ac12ed
fziadah24 <fziadah244@gmail.com>
2023-12-15 23:26:53 -0500

    verify thingys added

--- pages/webvilletunes/playlist.js
+++ pages/webvilletunes/playlist.js
@@ -1,28 +1,61 @@
-window.onload = init;
-
-
-function init() {
-	var button = document.getElementById("addButton");
-	button.onclick = handleButtonClick;
-	loadPlaylist();
+function setUpButtonHandler() {
+    let button = document.getElementById("addButton");
+    button.onclick = handleButtonClick;
 }
 
-function handleButtonClick(e) {
+function handleButtonClick() {
+    let textInput = document.getElementById("songTextInput");
+    let songName = textInput.value;
+    if (songName == "") {
+        alert("Please enter a song!");
+    } else {
+       console.log("Adding " + songName);
+        let li = document.createElement("li");
+		li.innerHTML = songName;
+		let ul = document.getElementById("playlist");
+		ul.appendChild(li);
+	      ul.style.backgroundColor = "#899ff0";
+	      ul.style.color = "black";
+	    save(songName);
+    }
+}
+function save(item) {
+	var playlistArray = getStoreArray("playlist");
+	playlistArray.push(item);
+	localStorage.setItem("playlist", JSON.stringify(playlistArray));
+}
 
-	var textInput = document.getElementById("songTextInput");
-	var songName = textInput.value;
+function loadPlaylist() {
+	var playlistArray = getSavedSongs();
+	var ul = document.getElementById("playlist");
+	if (playlistArray != null) {
+		for (var i = 0; i < playlistArray.length; i++) {
+			var li = document.createElement("li");
+			li.innerHTML = playlistArray[i];
+			ul.appendChild(li);
+		}
+	}
+}
 
-	if (songName == "") {
-		alert("Please enter a song");
+function getSavedSongs() {
+	return getStoreArray("playlist");
+}
+
+function getStoreArray(key) {
+	var playlistArray = localStorage.getItem(key);
+	if (playlistArray == null || playlistArray == "") {
+		playlistArray = new Array();
 	}
 	else {
-
-		var li = document.createElement("li");
-		li.innerHTML = songName;
-		var ul = document.getElementById("playlist");
-		ul.appendChild(li);
-
-
-		save(songName);
+		playlistArray = JSON.parse(playlistArray);
 	}
+	return playlistArray;
+}
+function setValidationLinks() {
+  let loc = window.location.href;
+  let HTMLvalidLinkStr = 'http://validator.w3.org/check?uri=' + loc;
+  let CSSvalidLinkStr = 'http://jigsaw.w3.org/css-validator/validator?uri=' +
+                        loc + '?profile=css3';
+  document.getElementById("vLink1").setAttribute("href", HTMLvalidLinkStr);
+  document.getElementById("vLink2").setAttribute("href", CSSvalidLinkStr);
 }
